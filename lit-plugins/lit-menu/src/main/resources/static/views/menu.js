93d044044a78b8b568df2173d5a7405912671e30
liul <liul@ipampas.com>
2020-03-17 17:36:23 +0800

    add menu

--- lit-plugins/lit-menu/src/main/resources/static/views/menu.js
+++ lit-plugins/lit-menu/src/main/resources/static/views/menu.js
@@ -1,105 +1,18 @@
-define(['Lit'], function (Lit) {
-    let tmpl = `
-<main class="aui-main">
-    <app-breadcrumb title="菜单管理"></app-breadcrumb>
-
-    <div class="aui-main__bd">
-        <el-card shadow="never">
-            <div slot="header">
-                <el-row>
-                    <el-col :span="4">
-                        <el-button type="primary" plain icon="el-icon-plus" @click="handleAdd('')"></el-button>
-                    </el-col>
-                    <el-col :span="12" :offset="2">
-                        <el-input v-model="keyword" placeholder="请输入搜索内容" v-on:keyup.native.enter="handleSearch">
-                            <el-button slot="append" icon="el-icon-search" @click="handleSearch"></el-button>
-                        </el-input>
-                    </el-col>
-                </el-row>
-            </div>
-
-            <!--<el-row class="b-bottom-1" style="height: 30px;">-->
-                <!--<el-col :span="4"><span class="ml-25 fz-lg">编码</span></el-col>-->
-                <!--<el-col :span="2"><span class="ml-15 fz-lg">图标</span></el-col>-->
-                <!--<el-col :span="4"><span class="ml-25 fz-lg">名称</span></el-col>-->
-                <!--<el-col :span="6"><span class="ml-15 fz-lg">url</span></el-col>-->
-                <!--<el-col :span="2"><span class="fz-lg">顺序号</span></el-col>-->
-                <!--<el-col :span="3"><span class="fz-lg">启用状态</span></el-col>-->
-                <!--<el-col :span="3"><span class="fz-lg">操作</span></el-col>-->
-            <!--</el-row>-->
-            <!--<el-tree :data="data"-->
-                     <!--:expand-on-click-node="false"-->
-                     <!--:filter-node-method="filterNode"-->
-                     <!--ref="menuTree">-->
-                <!--<div slot-scope="{ node, data }" style="width: 100%;height: 45px;border-bottom: 1px solid #ebeef5;">-->
-                    <!--<el-row type="flex" align="middle">-->
-                        <!--<el-col :span="4"><span>{{ data.code }}</span></el-col>-->
-                        <!--<el-col :span="2"><i :class="data.icon"></i></el-col>-->
-                        <!--<el-col :span="4"><span> {{ data.name }}</span></el-col>-->
-                        <!--<el-col :span="6"><span>{{ data.url }}</span></el-col>-->
-                        <!--<el-col :span="2"><span>{{ data.orderNum }}</span></el-col>-->
-                        <!--<el-col :span="3">-->
-                            <!--<el-switch v-model="data.enable" @change="handleChange(data.id)"></el-switch>-->
-                        <!--</el-col>-->
-                        <!--<el-col :span="3">-->
-                            <!--<el-button type="text" icon="el-icon-plus" @click="handleAdd(data)"></el-button>-->
-                            <!--<el-button type="text" icon="el-icon-edit" @click="handleEdit(data)"></el-button>-->
-                            <!--<el-button type="text" icon="el-icon-delete" @click="handleDelete(data.id)"></el-button>-->
-                        <!--</el-col>-->
-                    <!--</el-row>-->
-                <!--</div>-->
-            <!--</el-tree>-->
-            
-            <el-table :data="data" row-key="id" default-expand-all>
-                <el-table-column prop="code" label="编码"></el-table-column>
-                <el-table-column prop="icon" label="图标"></el-table-column>
-                <el-table-column prop="name" label="名称"></el-table-column>
-                <el-table-column prop="url" label="url"></el-table-column>
-                <el-table-column prop="orderNum" label="顺序号"></el-table-column>
-                <el-table-column prop="enable" label="启用状态"></el-table-column>
-                <el-table-column prop="" label="操作"></el-table-column>
-              </el-table>
-            
-        </el-card>
-    </div>
-
-    <el-dialog :title="editFormConfig.title" :visible.sync="editFormConfig.visible" :close-on-click-modal="false">
-        <el-form :model="editForm" label-width="100px" label-suffix=":">
-            <el-form-item label="父节点" v-if="editFormConfig.isAdd">
-                <span>{{editFormConfig.parent}}</span>
-            </el-form-item>
-            <el-form-item label="编码">
-                <el-input v-model="editForm.code"></el-input>
-            </el-form-item>
-            <el-form-item label="名称">
-                <el-input v-model="editForm.name"></el-input>
-            </el-form-item>
-            <el-form-item label="图标">
-                <el-input v-model="editForm.icon"></el-input>
-            </el-form-item>
-            <el-form-item label="url">
-                <el-input v-model="editForm.url"></el-input>
-            </el-form-item>
-            <el-form-item label="顺序号">
-                <el-input v-model="editForm.orderNum" type="number"></el-input>
-            </el-form-item>
-            <el-form-item label="备注">
-                <el-input type="textarea" :rows="2" v-model="editForm.remark"></el-input>
-            </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-            <el-button type="primary" @click="doEdit">确 定</el-button>
-            <el-button @click="editFormConfig.visible = false">取 消</el-button>
-        </div>
-    </el-dialog>
-</main>
-    `
+define(['text!/views/menu.html'], function (tmpl) {
     return {
         template: tmpl,
         data: function () {
             return {
                 data: [],
                 keyword: '',
+                columns: [
+                    { label: '名称', prop: 'name', width: '300px' },
+                    { label: '编码', prop: 'code', width: '200px' },
+                    { label: '图标', prop: 'icon', width: '100px' },
+                    { label: 'url', prop: 'url', width: '250px' },
+                    { label: '顺序号', prop: 'orderNum', width: '80px' },
+                    { label: '状态', prop: 'enable', width: '100px' }
+                ],
                 editFormConfig: {
                     visible: false,
                     isAdd: true,
@@ -118,12 +31,11 @@ define(['Lit'], function (Lit) {
             }
         },
         created() {
-            Lit.appendStyle('.el-tree-node__content {height: 45px;border-bottom: 1px solid #ebeef5;}')
             this.initData()
         },
         methods: {
             initData() {
-                Lit.httpRequest.get('/api/menu/tree').then(res => {
+                this.$http.get('/api/menu/tree').then(res => {
                     this.data = res.result || []
                 })
             },
@@ -150,7 +62,7 @@ define(['Lit'], function (Lit) {
             },
             doEdit() {
                 let method = this.editFormConfig.isAdd ? 'post' : 'put'
-                Lit.httpRequest.request(method, '/api/menu', this.editForm).then(res => {
+                this.$http.request(method, '/api/menu', this.editForm).then(res => {
                     if (res.success) {
                         this.$message.success(this.editFormConfig.title + '成功')
                         this.initData()
@@ -165,7 +77,7 @@ define(['Lit'], function (Lit) {
                     closeOnClickModal: false,
                     type: 'warning'
                 }).then(() => {
-                    Lit.httpRequest.delete('/api/menu/' + id,).then(res => {
+                    this.$http.delete('/api/menu/' + id,).then(res => {
                         if (res.success) {
                             this.$message.success('删除菜单成功')
                             this.initData()
@@ -177,7 +89,7 @@ define(['Lit'], function (Lit) {
                 })
             },
             handleChange(id) {
-                Lit.httpRequest.post("/api/menu/change/status/" + id).then(res => {
+                this.$http.post("/api/menu/change/status/" + id).then(res => {
                     if(!res.success) {
                         this.$message.error(res.message)
                     }
